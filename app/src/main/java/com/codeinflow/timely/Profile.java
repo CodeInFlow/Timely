cdd9c4989fce12b860f1ec4acb8a0f44adecc25a
Saurav singh <sauravsingh85079@gmail.com>
2021-10-30 03:56:33 +0530

    Add Profile java file

--- app/src/main/java/com/codeinflow/timely/Profile.java
+++ app/src/main/java/com/codeinflow/timely/Profile.java
@@ -2,6 +2,7 @@ package com.codeinflow.timely;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -11,6 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 
