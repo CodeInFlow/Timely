2d4dd1f6bfd0f08b9203bad3f1befa4b0b467c3e
Saurav singh <sauravsingh85079@gmail.com>
2021-10-30 02:37:56 +0530

    Add Intro 2 file

--- app/src/main/java/com/codeinflow/timely/Intro2.java
+++ app/src/main/java/com/codeinflow/timely/Intro2.java
@@ -4,21 +4,37 @@ package com.codeinflow.timely;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Intro2 extends Fragment {
 
-
+    Button btnStart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_intro2, container, false);
+        View v = inflater.inflate(R.layout.fragment_intro2, container, false);
+        btnStart = v.findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new Login();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frag_container1, fragment);
+                fragmentTransaction.commit();
+            }
+        });
+        return v;
     }
 
 }
