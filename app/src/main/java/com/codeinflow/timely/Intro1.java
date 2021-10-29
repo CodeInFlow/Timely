04a7adac3ae547dafab9f22d8700845b27d577a0
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-10-30 02:23:01 +0530

    Add 2nd Activity

--- app/src/main/java/com/codeinflow/timely/Intro1.java
+++ app/src/main/java/com/codeinflow/timely/Intro1.java
@@ -10,21 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Intro1 extends Fragment {
-
-
-    public Intro1() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_intro1, container, false);
     }
 
