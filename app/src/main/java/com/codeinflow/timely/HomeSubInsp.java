9b5e7ff5d2769ab67eae4a1fe1e6dffb26dda3ea
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-10-30 03:25:42 +0530

    Create 2nd and 3rd Home page

---
+++ app/src/main/java/com/codeinflow/timely/HomeSubInsp.java
@@ -0,0 +1,25 @@
+package com.codeinflow.timely;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeSubInsp extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_home_sub_insp, container, false);
+
+        return v;
+    }
+
+}
