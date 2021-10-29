332221a392a42e299f471a06d86b509a97e7478c
Saurav singh <sauravsingh85079@gmail.com>
2021-10-30 03:35:21 +0530

    Add Profile java file

--- app/src/main/java/com/codeinflow/timely/Profile.java
+++ app/src/main/java/com/codeinflow/timely/Profile.java
@@ -4,17 +4,81 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class Profile extends Fragment {
 
+    private FirebaseFirestore db;
+    TextView Name,desig,pnoNum,dob,issueDate,area,p_name;
+    ImageView imgPP,sign;
+    String a,b,c,d,e,f,g,h,i;
+    String currentuser;
+
      @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+         Name=v.findViewById(R.id.pName);
+         desig=v.findViewById(R.id.desig);
+         pnoNum=v.findViewById(R.id.ph_nos);
+         dob=v.findViewById(R.id.dob_nos);
+         issueDate=v.findViewById(R.id.issue_date);
+         p_name=v.findViewById(R.id.p_name);
+         area=v.findViewById(R.id.area);
+         imgPP=v.findViewById(R.id.pp_img);
+         sign=v.findViewById(R.id.sign);
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
+         currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+         db=FirebaseFirestore.getInstance();
+
+         if (currentuser.length() > 5) {
+             DocumentReference docRef = db.collection("Users").document(currentuser);
+             docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                 @Override
+                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                     if (task.isSuccessful()) {
+                         DocumentSnapshot document = task.getResult();
+                         if (document != null) {
+                             a = document.getString("name");
+                             b = document.getString("desig");
+                             c = document.getString("img");
+                             d = document.getString("dob");
+                             e = document.getString("issuedate");
+                             f = document.getString("shosigdetailsname");
+                             g = document.getString("shosigdetails");
+                             h = document.getString("pnonum");
+                             i = document.getString("sigimg");
+
+
+                             Name.setText(a);
+                             desig.setText(b);
+                             pnoNum.setText(h);
+                             dob.setText(d);
+                             issueDate.setText(e);
+                             p_name.setText(f);
+                             area.setText(g);
+                             Picasso.get().load(c).into(imgPP);
+                             Picasso.get().load(i).into(sign);
+
+                         } else {
+                             Log.d("LOGGER", "Error");
+                         }
+                     } else {
+                         Log.d("LOGGER", "get failed with ", task.getException());
+                     }
+                 }
+             });
+         }
+
+
+         return v;
+     }
 }
